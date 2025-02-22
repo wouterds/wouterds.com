f9976a26232e2ebf2c0a277dd3e042a6fe833724
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-02-22 10:26:38 +0100

    chore: remove jest leftover

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,4 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    environment: 'node',
-    include: ['src/**/*.spec.{ts,tsx}'],
-    silent: true,
-  },
 });
