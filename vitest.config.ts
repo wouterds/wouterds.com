a30d278f819f7902e26778587879230235b29633
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2025-02-22 10:12:02 +0100

    chore: vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.spec.{ts,tsx}'],
+    silent: true,
+  },
+});
